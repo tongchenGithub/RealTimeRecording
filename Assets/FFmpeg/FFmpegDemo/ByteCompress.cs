b1c53952b2d934e651204200b4c38871fb8e7a1f
tongchenGithub <1471080924@qq.com>
2021-11-25 09:22:42 +0800

    feature: 提交高效录像功能

---
+++ Assets/FFmpeg/FFmpegDemo/ByteCompress.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using UnityEngine;
+
+public class TestByteAttay : MonoBehaviour
+{
+    //gzip字节数组压缩  
+    public static byte[] gzipCompress(byte[] data)
+    {
+        try
+        {
+            MemoryStream ms = new MemoryStream();
+            GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true);
+            zip.Write(data, 0, data.Length);
+            zip.Close();
+            byte[] buffer = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(buffer, 0, buffer.Length);
+            ms.Close();
+            return buffer;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+
+    //gzip字节数组解压缩  
+    public static byte[] gzipDecompress(byte[] data)
+    {
+        try
+        {
+            MemoryStream ms = new MemoryStream(data);
+            GZipStream zip = new GZipStream(ms, CompressionMode.Decompress, true);
+            MemoryStream msreader = new MemoryStream();
+            byte[] buffer = new byte[0x1000];
+            while (true)
+            {
+                int reader = zip.Read(buffer, 0, buffer.Length);
+                if (reader <= 0)
+                {
+                    break;
+                }
+
+                msreader.Write(buffer, 0, reader);
+            }
+
+            zip.Close();
+            ms.Close();
+            msreader.Position = 0;
+            buffer = msreader.ToArray();
+            msreader.Close();
+            return buffer;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
